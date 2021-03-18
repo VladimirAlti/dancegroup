acef09b8d2de8c48a9cae3c25fcde8855d279791
VladimirAlti <vladimir.altiparmakov@gmail.com>
2021-03-18 17:56:59 +0100

    Initial project

---
+++ src/main/java/com/example/application/views/search/SearchView.java
@@ -0,0 +1,34 @@
+package com.example.application.views.search;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.example.application.views.main.MainView;
+import com.vaadin.flow.router.RouteAlias;
+
+@CssImport("./views/search/search-view.css")
+@Route(value = "search", layout = MainView.class)
+@RouteAlias(value = "", layout = MainView.class)
+@PageTitle("Search")
+public class SearchView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public SearchView() {
+        addClassName("search-view");
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        add(name, sayHello);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+    }
+
+}
